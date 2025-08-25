0c3dc10e5f17e69e3eac036ad47f77cc2a9af6ec
Anurag bhoite <anuragcr7778@gmail.com>
2025-08-25 23:09:56 +0530

    Worker updated

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -2,17 +2,16 @@ use diesel::prelude::*;
 use dotenvy::dotenv;
 use redisstreams::redis::Redis;
 use reqwest::StatusCode;
+use std::time::Duration;
 use std::{env, io::Error, time::Instant};
 use store::schema::website_tick;
 use store::{models::website::WebsiteTick, store::Store};
+use tokio::time::sleep;
 use uuid::Uuid;
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-
-    dotenv().ok();
-    
+async fn main_loop() -> Result<(), Error> {
     let region = env::var("REGION").map_err(|e| Error::new(std::io::ErrorKind::InvalidInput, e))?;
+
     let worker_id =
         env::var("WORKER_ID").map_err(|e| Error::new(std::io::ErrorKind::InvalidInput, e))?;
 
@@ -33,39 +32,53 @@ async fn main() -> Result<(), Error> {
     let mut str =
         Store::default().map_err(|e| Error::new(std::io::ErrorKind::ConnectionRefused, e))?;
 
-    let messages = r.x_read_group(region, worker_id).map_err(|e| {
-        Error::new(
-            std::io::ErrorKind::InvalidData,
-            format!("XREADGROUP Error {}", e),
-        )
-    })?;
+    loop {
+        let cloned_region = region.clone();
+        let cloned_worker_id = worker_id.clone();
 
-    match messages {
-        Some(s) => {
-            let streams = s.keys;
-            for stream in streams {
-                let stream_ids = stream.ids;
+        let messages = r
+            .x_read_group(cloned_region, cloned_worker_id)
+            .map_err(|e| {
+                Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    format!("XREADGROUP Error {}", e),
+                )
+            })?;
 
-                for stream_id in stream_ids {
-                    let map = stream_id.map;
-                    let url_value = map.get("url").unwrap();
-                    let website_id_value = map.get("id").unwrap();
+        match messages {
+            Some(s) => {
+                let streams = s.keys;
+                for stream in streams {
+                    let stream_ids = stream.ids;
 
-                    let url = redis::from_redis_value::<String>(url_value).unwrap();
-                    let website_id = redis::from_redis_value::<String>(website_id_value).unwrap();
+                    for stream_id in stream_ids {
+                        let map = stream_id.map;
+                        let url_value = map.get("url").unwrap();
+                        let website_id_value = map.get("id").unwrap();
 
-                    fetch_website(&mut str, url, website_id).await;
+                        let url = redis::from_redis_value::<String>(url_value).unwrap();
+                        let website_id =
+                            redis::from_redis_value::<String>(website_id_value).unwrap();
+
+                        fetch_website(&mut str, url, website_id).await;
+                    }
                 }
             }
+            None => {}
         }
-        None => {}
-    }
 
+        sleep(Duration::from_secs(3)).await;
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
+    main_loop().await?;
     Ok(())
 }
 
 async fn fetch_website(s: &mut Store, url: String, website_id: String) {
-
     let start_time = Instant::now();
 
     let res = reqwest::get(format!("https://{}", url)).await;
@@ -73,11 +86,8 @@ async fn fetch_website(s: &mut Store, url: String, website_id: String) {
     let total_time = start_time.elapsed().as_millis() as i32;
 
     match res {
-
         Ok(rps) => {
-
             if rps.status() == StatusCode::OK {
-
                 let website_tick = WebsiteTick {
                     id: Uuid::new_v4().to_string(),
                     response_time_ms: total_time,
@@ -91,7 +101,6 @@ async fn fetch_website(s: &mut Store, url: String, website_id: String) {
                     .returning(WebsiteTick::as_returning())
                     .get_result(&mut s.conn);
             } else {
-
                 let website_tick = WebsiteTick {
                     id: Uuid::new_v4().to_string(),
                     response_time_ms: total_time,
@@ -113,13 +122,13 @@ async fn fetch_website(s: &mut Store, url: String, website_id: String) {
                 response_time_ms: total_time,
                 status: "Unknown".to_owned(),
                 region_id: "1".to_owned(),
-                website_id
+                website_id,
             };
 
             let _ = diesel::insert_into(website_tick::table)
-            .values(website_tick)
-            .returning(WebsiteTick::as_returning())
-            .get_result(&mut s.conn);
+                .values(website_tick)
+                .returning(WebsiteTick::as_returning())
+                .get_result(&mut s.conn);
         }
     }
 }
